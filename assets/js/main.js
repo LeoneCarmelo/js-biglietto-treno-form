feb6c8ac9cf9bc430e949c5ae573786c4c29d5db
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-09 19:24:31 +0100

    Add ticket Style

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,26 +23,38 @@ const kmEl = document.getElementById('km')
 const ageEl = document.getElementById('age')
 const btnEl = document.querySelector('input[type="button"]')
 const resetEl = document.querySelector('input[type="reset"]')
-const ticketEl = document.querySelector('p')
-const ticketName = document.querySelector('span')
+const ticketEl = document.getElementById('ticket_cost')
+const ticketName = document.getElementById('nome_passeggero')
+const offertaEl = document.getElementById('offerta')
+const carrozzaEl = document.getElementById('carrozza')
+const capEl = document.getElementById('cap')
+
 
 
 
 /* Code */
 const costTotalKm = kmEl.value * priceKm;
 let ticketCost
+let ticketType
 
 btnEl.addEventListener('click', function() {
+ 
     if (ageEl.options.selectedIndex == 0 ) {
         ticketCost = costTotalKm - (costTotalKm * youngDiscount) / 100
+        ticketType = 'Tariffa giovani'
     } else if (ageEl.options.selectedIndex == 2) {
         ticketCost = costTotalKm - (costTotalKm * adultDiscount) / 100
+        ticketType = 'Tariffa over 65'
     } else {
         ticketCost = costTotalKm
+        ticketType = 'Tariffa standard'
     }
     
     ticketName.innerHTML = nameEl.value
-    ticketEl.innerHTML = `Il prezzo del biglietto è di ${ticketCost.toFixed(2)} €`
+    offertaEl.innerHTML = ticketType
+    carrozzaEl.innerHTML = Math.floor(Math.random() * 50)
+    capEl.innerHTML = Math.floor(Math.random() * 100000)
+    ticketEl.innerHTML = `${ticketCost.toFixed(2)} €`
 })
 
 resetEl.addEventListener('click', function() {
@@ -50,4 +62,7 @@ resetEl.addEventListener('click', function() {
     kmEl.value = ''
     ticketEl.innerHTML = ''
     ticketName.innerHTML = ''
+    offertaEl.innerHTML = ''
+    carrozzaEl.innerHTML = ''
+    capEl.innerHTML = ''
 })
